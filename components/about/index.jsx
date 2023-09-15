b8f2daefafe9175abd8d0f43f87b2b4792c60e07
Raghvendra1993 <raghavmdbn@gmail.com>
2023-09-15 12:20:20 +0530

    initial commit

---
+++ components/about/index.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Modal from "react-modal";
+import Testimonial from "./Testimonial";
+import Intro from "./Intro";
+import KnowledgeInterest from "./KnowledgeInterest";
+import PersonalInfo from "./PersonalInfo";
+import Resume from "./Resume";
+import Skills from "./Skills";
+
+Modal.setAppElement("#__next");
+
+const AboutMain = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="tokyo_tm_about">
+          <div className="tokyo_tm_title">
+            <div className="title_flex">
+              <div className="left">
+                <span>About</span>
+                <h3>About Me</h3>
+              </div>
+            </div>
+          </div>
+          {/* End title */}
+
+          <Intro />
+          <div className="tokyo_tm_short_info">
+            <PersonalInfo />
+          </div>
+          {/* End personal info */}
+
+          <div className="tokyo_tm_button" data-position="left">
+            {/* <a href="/img/cv.pdf" download>
+              <span>Download CV</span>
+            </a> */}
+            <a href="" >
+              <span>Download CV</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      
+
+      {/* <div className="tokyo_tm_progressbox">
+        <div className="container">
+          <div className="in">
+            <Skills />
+          </div>
+          
+        </div>
+        
+      </div> */}
+      
+
+      {/* <div className="tokyo_tm_skillbox">
+        <div className="container">
+          <div className="in">
+            <KnowledgeInterest />
+          </div>
+        </div>
+      </div> */}
+
+      {/* <div className="tokyo_tm_resumebox">
+        <div className="container">
+          <div className="in">
+            <Resume />
+          </div>
+        </div>
+      </div> */}
+      
+      {/* <div className="tokyo_tm_testimonials">
+        <div className="container">
+          <div className="tokyo_section_title">
+            <h3>Testimonials</h3>
+          </div>
+          <div className="list">
+            <Testimonial />
+          </div>
+        </div>
+      </div> */}
+     
+
+      
+    </>
+  );
+};
+
+export default AboutMain;
---
+++ data/servicesData.js
@@ -0,0 +1,287 @@
+module.exports = [
+  {
+    id: 1,
+    no: "01",
+    title: "Taxation Services",
+    text: `Starting to deal with taxes can be confusing, but don't worry. Our experienced team of tax experts is here to help. They'll not only guide you through the complicated tax rules but also make sure you follow all the tax laws correctly while finding the best tax plan for you. We offer a wide range of services, including:`,
+    largeImg: "/img/news/1.jpg",
+    largeTitle: "",
+    poster: "",
+    date: "",
+    descriptions: (
+      <>
+        <h5>For Individuals:</h5>
+        <p>
+          Taxes are a constant in every individual's life, and managing them effectively can make a substantial difference in your financial well-being. Our comprehensive individual taxation services include:
+        </p>
+        <p>
+          Tax Preparation: We simplify the annual tax filing process by accurately preparing your returns, optimizing deductions and credits, and ensuring compliance with tax laws.
+        </p>
+        <p>
+          Tax Planning: Beyond tax season, we provide strategic tax planning to help you make informed financial decisions throughout the year, from investments to retirement planning.
+        </p>
+        <p>
+          Tax Compliance: Staying on the right side of tax laws is vital. We guide you through your tax obligations, file returns punctually, and help you avoid potential penalties or audits.
+        </p>  
+        <p>
+          Income Tax Optimization: Our experts explore opportunities to minimize your tax liabilities legally, identifying deductions, credits, and tax-efficient investments to help you retain more of your income.
+        </p>
+
+<h5>For Companies:</h5>
+<p>
+  In the corporate world, tax considerations are pivotal to financial success. Our specialized tax services for companies include:
+</p>
+<p>
+  Corporate Tax Planning: We develop customized tax strategies to optimize your corporate tax structure, minimizing liabilities while maximizing available benefits.
+</p> 
+<p>
+  Tax Compliance: We ensure your company adheres to tax laws and regulations, from filing annual returns to managing payroll taxes and more.
+</p> 
+<p>
+  Tax Incentive Optimization: Explore available tax incentives and credits to boost your company's financial performance and competitiveness.
+</p>  
+<p>
+  Business Expansion: We assist in tax planning when expanding your operations, ensuring your business scales smoothly while maintaining tax efficiency.
+</p>
+
+
+<h5>For Trusts:</h5>
+<p>
+  Trusts are complex financial entities with distinct tax implications. Our trust taxation services include:
+</p>
+<p>
+  Trust Tax Reporting: Accurate and timely reporting of trust income and distributions in compliance with tax laws.
+</p>
+<p>
+  Trustee Assistance: Our experts work closely with trustees to manage tax-related responsibilities effectively.
+</p>
+<p>
+  Trustee and Beneficiary Tax Guidance: Providing guidance to trustees and beneficiaries on the tax implications of trust distributions.
+</p>
+
+
+</>
+),
+},
+  {
+    id: 2,
+    no: "02",
+    title: "Company Trust and SMSF Setup",
+    text: `Starting a company, trust, or a self-managed superannuation fund (SMSF) is an important part of securing your financial future. We provide a wide range of services to help you set up these financial structures carefully and skillfully. Our goal is to make sure they align perfectly with your specific financial goals.
+    `,
+    largeImg: "/img/news/2.jpg",
+    largeTitle: "",
+    poster: "",
+    date: "",
+    descriptions: (
+      <>
+        
+        <h5>Company Setup:</h5>
+        <p>
+          Establishing a company is more than just paperwork; it's about creating a business entity that aligns with your vision. Our company setup services encompass:
+        </p>
+        <p>
+          Business Structure Selection: Expert guidance on choosing the most suitable business structure, whether it's a sole proprietorship, partnership, or a company.
+        </p>
+        <p>
+          Incorporation Services: Swift and efficient company registration, ensuring compliance with all relevant regulatory bodies
+        </p>
+        <p>
+          Corporate Governance: Assistance in establishing governance structures, including shareholder agreements, bylaws, and operating agreements.
+        </p>
+        <p>
+        Tax Optimization: Strategic tax planning to maximize the benefits of your chosen company structure
+      </p>
+      <h5>Trust Setup:</h5>
+      <p>
+        Trusts are versatile vehicles for asset protection, estate planning, and achieving specific financial objectives. Our trust setup services include:
+      </p>
+      <p>
+        Trustee Selection: Expertise in selecting the right trustee(s) to oversee the trust's operations.
+      </p>
+      <p>
+        Trust Deed Creation: Crafting a meticulously drafted trust deed that aligns with your trust's objectives and meets all legal requirements.
+      </p>
+      <p>
+        Asset Transfer: Guidance on transferring assets into the trust with precision and compliance
+      </p>
+      <p>
+        Tax Efficiency: Developing tax-efficient strategies to optimize the benefits of your trust structure.
+      </p>
+      <h5>SMSF Setup:</h5>
+      <p>
+      Taking control of your retirement savings through an SMSF requires careful planning and ongoing compliance. Our SMSF setup services cover:
+      </p>
+      <p>
+        SMSF Establishment: Comprehensive setup of your SMSF, including trust deed creation and regulatory compliance.
+    </p>
+    <p>
+      Compliance Assurance: Ensuring ongoing SMSF compliance with Australian Taxation Office (ATO) regulations.
+    </p>
+  </>
+    ),
+  },
+  {
+    id: 3,
+    no: "03",
+    title: "Payroll Services:",
+    text: `Effective payroll management is pivotal, serving as the linchpin of employee satisfaction and regulatory adherence. Our comprehensive solutions include:`,
+    largeImg: "/img/news/3.jpg",
+    largeTitle: "",
+    poster: "",
+    date: "",
+    descriptions: (
+      <>
+      <h5>Payroll processing and reporting</h5>
+        <p>
+          Handling employee pay and keeping records is like the central hub of keeping your workforce organized and following the rules. Our special services include making sure we pay employees the right way and creating detailed reports to make sure everything is correct and follows the rules. 
+        </p>
+      <h5>Superannuation compliance</h5>
+        <p>
+          Making sure that employers follow the rules for retirement savings (superannuation) is really important for being responsible with money. Our special services for superannuation help businesses understand and follow all the complicated rules while making sure everything is clear, accurate, and legal.    
+        </p>
+      <h5>PAYG withholding calculations</h5>
+      <p>
+        Calculating PAYG (Pay As You Go) withholding is a big part of managing payroll responsibly. It means figuring out how much tax to take out of employees' paychecks before giving them the money. Our special PAYG services help businesses do this correctly by handling all the complicated tax rules, making payroll easier, and keeping everything legal.
+      </p>
+      <h5>Single Touch Payroll (STP) compliance</h5>
+      <p>
+        Following Single Touch Payroll (STP) rules is an important part of managing payroll today. It changes how businesses tell the government about their employee pay. Our special STP services are here to make this reporting easy, accurate, and smoothly connect your payroll with what the government needs.
+      </p>
+      </>
+    ),
+  },
+  {
+    id: 4,
+    no: "04",
+    title: "Efficient Bookkeeping Services: Your Financial Backbone",
+    text: `Bookkeeping is like the foundation of a strong financial plan for any business, no matter its size. It's all about keeping track of and organizing the money stuff, like income and expenses. This helps businesses make smart decisions, meet their financial responsibilities, and reach their money goals.`,
+    largeImg: "/img/news/4.jpg",
+    largeTitle: "",
+    poster: "",
+    date: "",
+    descriptions: (
+      <>
+      <h5>Our Bookkeeping Services:</h5>
+        <p>
+          Accurate Record-Keeping: We meticulously record all financial transactions, including income, expenses, purchases, and sales, ensuring your financial records are up-to-date and error-free 
+        </p>
+        <p>
+          Bank Reconciliation: We reconcile your bank statements with your financial records, identifying discrepancies and helping prevent errors or fraud.
+        </p>
+        <p>
+          Financial Statement Preparation: We prepare comprehensive financial statements, including balance sheets, income statements, and cash flow statements, to provide you with a clear overview of your financial position.
+        </p>
+        <p>
+          Expense Tracking: We track your business expenses, categorizing them to provide insights into where your money is going and helping you make informed spending decisions.
+        </p>
+        <p>
+          Accounts Receivable and Payable: We manage your accounts receivable, ensuring you get paid on time, and handle accounts payable, so you can manage your bills efficiently.
+        </p>
+        <p>
+          Tax Preparation Support: Our organized records make tax preparation smoother, helping you meet your tax obligations and potentially reduce your tax liability.
+        </p>
+        <p>
+          Financial Analysis: We provide insights into your financial data, enabling you to identify trends, make informed decisions, and plan for the future.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 5,
+    no: "05",
+    title: "Specialized Accounting Services for Not-for-Profit Organizations: Nurturing Your Mission",
+    text: `Managing money for non-profit groups is special and super important. It's not just about numbers; it's about using money wisely to do good things for your community or cause. Our special money services for non-profit groups are here to help you stay financially healthy and make sure you're clear, following the rules, and accountable for what you do with your money.`,
+    largeImg: "/img/news/1.jpg",
+    largeTitle: "Our Comprehensive Services:",
+    poster: "",
+    date: "",
+    descriptions: (
+      <>
+        <p>
+          Financial Analysis: We provide insights into your financial data, enabling you to identify trends, make informed decisions, and plan for the future.
+        </p>
+        <p>
+          Fund Accounting: We implement and manage fund accounting systems to track income, expenses, and assets for specific programs or projects, ensuring that donor and grant funds are allocated correctly.
+        </p>
+        <p>
+          Grant Management: We help you manage grants, ensuring that funds are used as intended, tracking expenses, and providing accurate reporting to grantors.
+        </p>
+        <p>
+          Compliance and Regulatory Reporting: We ensure your organization complies with the latest tax regulations, filings, and reporting requirements, maintaining your tax-exempt status.
+        </p>
+        <p>
+          Internal Controls: Establishing robust internal controls safeguards your organization against fraud and mismanagement, protecting both your assets and your reputation.
+        </p>
+        <p>
+          Effective bookkeeping is not just about crunching numbers; it's about providing you with the financial clarity and stability needed to thrive. Let us be your trusted partner in maintaining your financial backbone. Contact us today to discuss your bookkeeping needs and how we can help streamline your financial operations, so you can focus on what you do best—running your business.
+        </p>
+        <p>
+          At Wolf Street Accounting, we care about building strong relationships with our clients. We want to understand your goals and give you advice that helps you succeed financially. Whether you're just starting a business, have a small one, or run a big company, we have the knowledge to guide you through the complicated world of finance. Contact us today, and you'll see how working with our friendly and client-focused accounting firm can make a big difference to your financial journey.
+        </p>
+
+      </>
+    ),
+  },
+  {
+    id: 6,
+    no: "06",
+    title: "Financial Reporting:",
+    text: `Financial reporting is like a financial diary that helps individuals, businesses, and organizations keep track of their money matters. It's a structured way of documenting and summarizing financial activities over a specific period, usually monthly, quarterly, or annually. The primary goal is to provide a clear and accurate picture of a person's or entity's financial health. Here's how it works:`,
+    largeImg: "/img/news/2.jpg",
+    largeTitle: "",
+    poster: "",
+    date: "",
+    descriptions: (
+      <>
+        <p>
+          Income Statement: This part of financial reporting is like a snapshot of your earnings and expenses over a given period. It tells you how much money you've made (income) and how much you've spent (expenses). The difference between the two is your net income or loss.
+        </p>
+        <p>
+          Balance Sheet: Think of the balance sheet as a snapshot of what you own (assets) and what you owe (liabilities) at a particular moment. Your assets could include things like cash, investments, or property, while liabilities might be debts like loans or credit card balances. The balance sheet shows whether you have more assets than liabilities, which is a good sign.
+        </p>
+        <p>
+          Cash Flow Statement: This statement is all about tracking how cash flows in and out of your pocket or business. It includes operating cash flow (money from daily activities), investing cash flow (money spent on assets), and financing cash flow (money from loans or investments).
+        </p>
+        <p>
+          Financial reporting helps you assess your financial stability, make informed decisions, and communicate your financial status to others, such as investors or creditors. It's like taking a snapshot of your financial journey and understanding where you stand.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 7,
+    no: "07",
+    title: "Budgeting:",
+    text: `Budgeting is like having a game plan for your finances. It's a proactive way to manage your money by setting financial goals and deciding how you'll allocate your income to meet those goals. Here's how budgeting works:`,
+    largeImg: "/img/news/2.jpg",
+    largeTitle: "",
+    poster: "",
+    date: "",
+    descriptions: (
+      <>
+        <p>
+          Setting Goals: Start by identifying your financial goals, whether it's saving for a vacation, paying off debt, or building an emergency fund. These are like the destinations you want to reach.
+        </p>
+        <p>
+          Income and Expenses: Calculate your monthly income and list all your expenses. Expenses can be divided into fixed (like rent or mortgage) and variable (like groceries or entertainment).
+        </p>
+        <p>
+          Creating a Budget: Based on your goals and income, create a budget that outlines how much you'll allocate to each expense category. This is like planning how much to spend in each area of your life.
+        </p>
+        <p>
+          Tracking Expenses: Keep a close eye on your spending throughout the month. You can use apps or spreadsheets to help you. This is like checking your progress on a journey and making adjustments as needed. 
+        </p>
+        <p>
+          Reviewing and Adjusting: At the end of the month, compare your actual spending to your budget. If you overspent in one category, figure out how to compensate in the next month. If you have extra funds, consider saving or investing them.
+        </p>
+        <p>
+          Budgeting helps you take control of your finances, prioritize your goals, and ensure that you're using your money wisely. It's like having a map for your financial journey, helping you reach your desired destinations efficiently.
+        </p>
+        <p>
+          In summary, financial reporting gives you a snapshot of your financial past and present, while budgeting helps you plan for your financial future. Together, they are powerful tools for managing your money and achieving your financial goals
+        </p>
+      </>
+    ),
+  },
+];
