5eb7775042d157b715dadf341cf0db0a09335c54
Raghvendra1993 <raghavmdbn@gmail.com>
2023-12-07 13:42:05 +0530

    Add sitemap

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -7,6 +7,8 @@ const Seo = ({ pageTitle, }) => {
         <title>{pageTitle || `Wolf Street Accountant`}</title>
         <meta name="description" content="We offer comprehensive financial planning, investment management, tax consulting, and more. Let us help you secure your financial future today"/>
         <meta name="keywords" content="Taxation Services, Company Trust and SMSF Setup, Payroll Services, Bookkeeping Services, Accounting Services, Financial Reporting, Budgeting" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> 
       </Head>
     </>
 
