616c0d606b0d1780a6b362156c62a0f1bf4469ec
Raghvendra1993 <raghavmdbn@gmail.com>
2023-12-11 20:02:08 +0530

    Page Title google metatage

--- pages/home.js
+++ pages/home.js
@@ -10,12 +10,12 @@ const index = () => {
     <div>
       <Head>
         <title>Home Page</title>
-        <meta name="google-site-verification" content="pqAUqYIutNRklsrdGGkeFoYiCHTXKsIPZ8S71f0ipkk" />
+        {/* <meta name="google-site-verification" content="pqAUqYIutNRklsrdGGkeFoYiCHTXKsIPZ8S71f0ipkk" /> */}
         <meta name="description" content="We offer comprehensive financial planning, investment management, tax consulting, and more. Let us help you secure your financial future today"/>
         <meta name="keywords" content="Taxation Services, Company Trust and SMSF Setup, Payroll Services, Bookkeeping Services, Accounting Services, Financial Reporting, Budgeting" />
       </Head>
     </div>
-      <Seo pageTitle="Home" />
+      <Seo pageTitle="Wolf Street Accountant" />
       {/* En Seo Page Title */}
 
       <Sidebar />
